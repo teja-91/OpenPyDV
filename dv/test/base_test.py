bec777e91cec1f98b576f77a3b7af31ffb297380
Raviteja Chatta <crteja@lowrisc.org>
2023-07-07 14:50:21 +0100

    Drive clock and reset from base test
    Signed-off-by: Raviteja Chatta <crteja@lowrisc.org>

--- dv/test/base_test.py
+++ dv/test/base_test.py
@@ -4,13 +4,10 @@ from cocotb.triggers import Timer
 
 from uvm import *
 from env.i2c_env import i2c_env
-
-# from tb_env import tb_env
-
-# Set to True to allow VIPs (drivers/monitors) raise/drop objections
-#hier_objection = False
+from env.clk_rst_if import clk_rst_if
 
 
+@uvm_component_utils
 class base_test(UVMTest):
 
     def __init__(self, name, parent=None):
@@ -32,41 +29,27 @@ class base_test(UVMTest):
         cs_ = UVMCoreService.get()
         self.top = cs_.get_root()
         self.top.print_topology()
+        clk_rst_if = []
         # get dut handle
-        dut = None
-        UVMConfigDb.set(self, "*", "dut", dut)
+        if UVMConfigDb.get(self, "*", "clk_rst_if", clk_rst_if):
+          UVMConfigDb.get(self, "*", "clk_rst_if", clk_rst_if)
+        else:
+          uvm_error(self.get_name(), "clk_rst_if not set in test top")
+        clk_rst_if[0].clk_period_ns = 20;
+        cocotb.start_soon(clk_rst_if[0].start_clock())
+        await clk_rst_if[0].assert_reset(20)
         await Timer(2000, "NS")
         phase.drop_objection(self, "base_test_end")
         
 
-    # def check_phase(self, phase):
-    #     self.error = self.env.has_errors()
-    #     if self.error:
-    #         uvm_fatal("TEST FAILED", "check_phase of test threw fatal")
-
-
-uvm_component_utils(base_test)
-
-
-async def do_reset_and_start_clocks(dut):
-    clk_i = 0
-    await Timer(100, "NS")
-    rst_ni = 0
-    await Timer(200, "NS")
-    rst_ni = 1
-    clk_i = 0
-    await Timer(500, "NS")
-    cocotb.start_soon(Clock(dut.clk_i, 50, "NS").start())
-
-
 @cocotb.test()
 async def test_i2c_basic_clock_and_reset(dut):
 
-    # UVMConfigDb.set(None, "env", "vif", ctl)
-    # UVMConfigDb.set(None, "env.apb", "vif", apb_vif)
-    # UVMConfigDb.set(None, "env.vip", "vif", vip0)
+    # Set clk_rst_if in UVM Config DB
+    i2c_clk_rst_if = clk_rst_if(dut, "", "clk_i", "rst_ni")
+    i2c_clk_rst_if.init()
 
-    cocotb.start_soon(do_reset_and_start_clocks(dut))
-    UVMConfigDb.set(None, "*", "dut", dut)
+    # cocotb.start_soon(do_reset_and_start_clocks(dut))
+    UVMConfigDb.set(None, "*", "clk_rst_if", i2c_clk_rst_if)
 
     await run_test("base_test")
